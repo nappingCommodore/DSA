20b6d5b2e9ec26483df196e9dd337c06da0754f4
Nitish Singh <8607280+nappingCommodore@users.noreply.github.com>
2024-05-04 05:34:48 +0000

    Adding Primitive data types example

---
+++ Datastructure/Primitive.java
@@ -0,0 +1,37 @@
+// Example of primitive datatypes in java
+public class Primitive {
+    public static void main(String[] args) {
+        // byte
+        byte byteVar = 127;
+        System.out.println("byteVar: " + byteVar);
+
+        // short
+        short shortVar = 32767;
+        System.out.println("shortVar: " + shortVar);
+
+        // int
+        int intVar = 2147483647;
+        System.out.println("intVar: " + intVar);
+
+        // long
+        long longVar = 9223372036854775807L;
+        System.out.println("longVar: " + longVar);
+
+        // float
+        float floatVar = 3.4028235E38f;
+        System.out.println("floatVar: " + floatVar);
+
+        // double
+        double doubleVar = 1.7976931348623157E308;
+        System.out.println("doubleVar: " + doubleVar);
+
+        // char
+        char charVar = 'A';
+        System.out.println("charVar: " + charVar);
+
+        // boolean
+        boolean booleanVar = true;
+        System.out.println("booleanVar: " + booleanVar);
+    }
+}
+
